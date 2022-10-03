4f50e38b288955c27c6d3b7e37e52fa8d5e2c1ce
NachoPiccoli <iepiccoli@hotmail.com>
2022-10-03 13:55:16 -0300

    Agregado funcion inicioP

--- ProyectoCoder2/ProyectoCoder2/urls.py
+++ ProyectoCoder2/ProyectoCoder2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppCoder2.views import mostrar_inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('inicio/', mostrar_inicio),
 ]
+
