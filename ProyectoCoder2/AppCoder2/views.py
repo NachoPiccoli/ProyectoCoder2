b81701012997862937333bec9b6cd3233c5678c1
NachoPiccoli <iepiccoli@hotmail.com>
2022-10-03 16:59:43 -0300

    Agrego templates con html mas

--- ProyectoCoder2/AppCoder2/views.py
+++ ProyectoCoder2/AppCoder2/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 #from django.http import HttpResponse # ya no lo uso más
 
 from django.shortcuts import render
+from AppCoder2.models import Estudiante
 
 # Create your views here.
 
 def mostrar_inicio (request):
-    return render(request, "AppCoder2/inicio.html")
+
+    estudiante=Estudiante(nombre="Juan", apellido="Perez", email="ajs@hotmail.com")
+    contexto= {"estudiante_1":estudiante}
+
+    return render(request, "AppCoder2/inicio.html",contexto)
     
     #return HttpResponse ("Hola Mundo ProyectoPython2")
